c908e4da276d06828a2cae584c0bd7f6004dae55
xukexin <moyunrz@163.com>
2025-06-23 17:45:53 +0800

    添加时间周期

--- src/services/service.rs
+++ src/services/service.rs
@@ -197,6 +197,9 @@ impl Service {
             "1d" => pd = Period::Day,
             "1w" => pd = Period::Week,
             "15m" => pd = Period::FifteenMinute,
+            "2h" => pd = Period::TwoHour,
+            "3h" => pd = Period::ThreeHour,
+            "4h" => pd = Period::FourHour,
             "5m" => pd = Period::FiveMinute,
             _ => pd = Period::UnknownPeriod
         }
